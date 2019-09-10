bfab8d005b174100751211360453a44488ba410d
Miguel Carrilho <mangelott@gmail.com>
2019-09-10 19:27:29 +0100

    LIS-FTWD-MiguelCarrilho-156

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 'use strict';
 
-const { join } = require('path');
+const {
+  join
+} = require('path');
 const express = require('express');
 const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 const serveFavicon = require('serve-favicon');
-
+const mongoose = require('mongoose');
 const indexRouter = require('./routes/index');
+const session = require("express-session");
+const LocalStrategy = require("passport-local").Strategy;
 const passportRouter = require("./routes/passport");
-
+const passport=require('passport');
 const app = express();
+const User = require('./models/user');
+const bcrypt=require('bcrypt');
 
 // Setup view engine
 app.set('views', join(__dirname, 'views'));
@@ -19,7 +25,9 @@ app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(serveFavicon(join(__dirname, 'public/images', 'favicon.ico')));
 app.use(express.static(join(__dirname, 'public')));
@@ -29,6 +37,25 @@ app.use(sassMiddleware({
   outputStyle: process.env.NODE_ENV === 'development' ? 'nested' : 'compressed',
   sourceMap: true
 }));
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/', passportRouter);
