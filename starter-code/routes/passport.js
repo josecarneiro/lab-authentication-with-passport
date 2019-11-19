cce7359062c90054891b7985017595de6339f71b
JoaoPedro13 <jpc130@gmail.com>
2019-11-19 15:58:28 +0000

    done

--- starter-code/routes/passport.js
+++ starter-code/routes/passport.js
@@ -1,25 +1,56 @@
-'use strict';
+"use strict";
 
-const { Router } = require('express');
+const { Router } = require("express");
 const passportRouter = Router();
 
 // Require user model
-
+const User = require("../models/user");
 // Add bcrypt to encrypt passwords
-
+const bcryptjs = require("bcryptjs");
 // Add passport
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
-const ensureLogin = require('connect-ensure-login');
+const ensureLogin = require("connect-ensure-login");
 
 passportRouter.get(
-  '/private-page',
+  "/private-page",
   ensureLogin.ensureLoggedIn(),
   (req, res, next) => {
     const user = req.user;
-    res.render('passport/private', {
+    res.render("passport/private", {
       user
     });
   }
 );
+
+passportRouter.get("/sign-up", (req, res, next) => {
+  res.render("passport/signup");
+});
+
+passportRouter.post(
+  "/sign-up",
+  passport.authenticate("sign-up", {
+    successRedirect: "/private-page",
+    failureRedirect: "/sign-up"
+  })
+);
+
+passportRouter.get("/sign-in", (req, res, next) => {
+  res.render("passport/login");
+});
+
+passportRouter.post(
+  "/sign-in",
+  passport.authenticate("sign-in", {
+    successRedirect: "/private-page",
+    failureRedirect: "/sign-in"
+  })
+);
+
+passportRouter.post("/sign-out", (req, res, next) => {
+  req.logout();
+  res.redirect("/");
+});
 
 module.exports = passportRouter;
