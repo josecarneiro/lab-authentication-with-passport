aee4bdbb06edf89640f907f9b27aefb509e43e5d
Lissy Gundersen <user@Users-MacBook-Pro.local>
2019-11-19 17:10:38 +0000

    completed,I think

--- starter-code/routes/passport.js
+++ starter-code/routes/passport.js
@@ -1,19 +1,18 @@
 'use strict';
 
-const { Router } = require('express');
+const {
+  Router
+} = require('express');
 const passportRouter = Router();
-
-// Require user model
 
 // Add bcrypt to encrypt passwords
 
-// Add passport
-
+const passport = require('passport');
 const ensureLogin = require('connect-ensure-login');
 
 passportRouter.get(
-  '/private-page',
-  ensureLogin.ensureLoggedIn(),
+  '/private',
+  ensureLogin.ensureLoggedIn('autentication/login'),
   (req, res, next) => {
     const user = req.user;
     res.render('passport/private', {
@@ -21,5 +20,34 @@ passportRouter.get(
     });
   }
 );
+
+// Add a new route to your passportRouter.js file with the path /signup and point it to your views/passport/signup.hbs file.
+passportRouter.get('/signup', (req, res, next) => {
+  res.render('passport/signup');
+});
+
+passportRouter.post(
+  '/signup',
+  passport.authenticate('signup', {
+    successRedirect: '/private',
+    failureRedirect: '/signup'
+  })
+);
+
+
+// let's add one GET route to our router to display the login page. views/passport/login.hbs is empty so let's fill it with some login form. Once we have the form, let's add another route to the router to receive that data and log the user in. The form should make a POST request to /login.
+
+passportRouter.get('/login', (req, res, next) => {
+  res.render('passport/login');
+});
+
+passportRouter.post(
+  '/login',
+  passport.authenticate('login', {
+    successRedirect: '/private',
+    failureRedirect: '/login'
+  })
+);
+
 
 module.exports = passportRouter;
