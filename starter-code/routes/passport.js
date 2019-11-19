82d96fb475a9480e57d782e1fc06f797ac7da02f
Alntjan <oliveiracatarino88@gmail.com>
2019-11-19 15:04:04 +0000

    Last Missing Iterations

--- starter-code/routes/passport.js
+++ starter-code/routes/passport.js
@@ -1,13 +1,39 @@
 'use strict';
 
-const { Router } = require('express');
+const {
+  Router
+} = require('express');
 const passportRouter = Router();
 
 // Require user model
 
 // Add bcrypt to encrypt passwords
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 // Add passport
+passportRouter.get('/signup', (req, res, next) => {
+  res.render('passport/signup');
+});
+
+passportRouter.post(
+  '/signup',
+  passport.authenticate('signup', {
+    successRedirect: '/private-page',
+    failureRedirect: '/signup'
+  })
+);
+
+passportRouter.get('/login', (req, res, next) => {
+  res.render('passport/login');
+});
+
+passportRouter.post('/login', (req, res, next) => {
+  passport.authenticate('login', {
+    successRedirect: '/private-page',
+    failureRedirect: '/'
+  })
+});
 
 const ensureLogin = require('connect-ensure-login');
 
