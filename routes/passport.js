9aed6cbfe89f084b108db4d5ed6f01c18f42a924
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-02-19 16:55:30 +0000

    post not working

--- routes/passport.js
+++ routes/passport.js
@@ -1,13 +1,36 @@
 'use strict';
 
 const { Router } = require('express');
-const passportRouter = Router();
-
-// Require user model
-
-// Add bcrypt to encrypt passwords
+const passportRouter = new Router();
 
 // Add passport
+const passport = require('passport');
+
+
+//SIGN IN:
+passportRouter.get('/sign-in', (req, res, next) => {
+  res.render('passport/sign-in');
+});
+
+passportRouter.post(
+  '/sign-in',
+  passport.authenticate('sign-in',{
+    successRedirect: '/',
+    failureRedirect: '/passport/sign-in'
+  })
+);
+
+//SIGN UP
+passportRouter.get('/sign-up', (req, res, next) => {
+  res.render('passport/sign-up');
+});
+
+passportRouter.post('/sign-up',
+passport.authenticate('sign-up',{
+  successRedirect: '/',
+  failureRedirect: '/passport/sign-up'
+  })
+);
 
 const ensureLogin = require('connect-ensure-login');
 
