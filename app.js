5a39bf30934430edb3185fb55cef8f8eb6816220
TemidoRocha <manueltemidorocha@gmail.com>
2020-02-19 16:55:48 +0000

    lab solved

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const createError = require('http-errors');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 const serveFavicon = require('serve-favicon');
+const passUserToTemplate = require('./middleware/pass-user-to-template');
+
+const mongoose = require('mongoose');
+const expressSession = require('express-session');
+const ConnectMongo = require('connect-mongo');
+
+const mongoStore = ConnectMongo(expressSession);
+
+const passport = require('passport');
 
 const indexRouter = require('./routes/index');
 const passportRouter = require('./routes/passport');
@@ -29,6 +38,31 @@ app.use(
 );
 app.use(serveFavicon(join(__dirname, 'public/images', 'favicon.ico')));
 app.use(express.static(join(__dirname, 'public')));
+
+app.use(
+  expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 15 * 24 * 60 * 60 * 1000
+    },
+    store: new mongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60
+    })
+  })
+);
+
+// Initiate passport middleware before mounting routers and after mounting express-session
+
+require('./passport-config');
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passUserToTemplate);
+
 
 app.use('/', indexRouter);
 app.use('/', passportRouter);
--- routes/passport.js
+++ routes/passport.js
@@ -1,21 +1,61 @@
 'use strict';
 
 const { Router } = require('express');
-const passportRouter = Router();
+
+const passportRouter = new Router();
 
 // Require user model
+const bcryptjs = require('bcryptjs');
 
 // Add bcrypt to encrypt passwords
+const User = require('./../models/user');
+
+const routeGuard = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    next(new Error('USER_NOT_AUTHORIZED'));
+  }
+};
 
 // Add passport
+const passport = require('passport');
 
 const ensureLogin = require('connect-ensure-login');
 
-passportRouter.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res, next) => {
+passportRouter.get('/passport/private-page', ensureLogin.ensureLoggedIn(), (req, res, next) => {
   const user = req.user;
   res.render('passport/private', {
     user
   });
+});
+
+passportRouter.get('/sign-in', (req, res, next) => {
+  res.render('passport/sign-in');
+});
+
+passportRouter.post(
+  '/sign-in',
+  passport.authenticate('sign-in', {
+    successRedirect: '/',
+    failureRedirect: '/passport/sign-in'
+  })
+);
+
+passportRouter.get('/sign-up', (req, res, next) => {
+  res.render('passport/sign-up');
+});
+passportRouter.post(
+  '/sign-up',
+  passport.authenticate('sign-up', {
+    successRedirect: '/',
+    failureRedirect: '/passport/sign-up'
+  })
+);
+
+passportRouter.post('/sign-out', (req, res, next) => {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = passportRouter;
