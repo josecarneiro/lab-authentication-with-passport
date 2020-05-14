d04a9940afbf24eb25c291f3ca1bcbb29393f276
karenrogersn <56484176+karenrogersn@users.noreply.github.com>
2020-05-14 20:21:04 +0100

    Completed Lab

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 'use strict';
 
+// 1. Mount the passport middleware on your app.
+// 2. Configure the serialization and deserialization process
+// 3. Configure passport strategies
+
 const { join } = require('path');
 const express = require('express');
 const createError = require('http-errors');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 const serveFavicon = require('serve-favicon');
-
+const connectMongo = require('connect-mongo');
+const mongoose = require('mongoose');
+const expressSession = require('express-session');
+const bindUserToViewLocals = require('./middleware/bind-user-to-view-locals');
 const indexRouter = require('./routes/index');
 const authenticationRouter = require('./routes/authentication');
+
+const passport = require('passport');
 
 const app = express();
 
@@ -22,13 +31,46 @@ app.use(
   sassMiddleware({
     src: join(__dirname, 'public'),
     dest: join(__dirname, 'public'),
-    outputStyle: process.env.NODE_ENV === 'development' ? 'nested' : 'compressed',
+    outputStyle:
+      process.env.NODE_ENV === 'development' ? 'nested' : 'compressed',
     force: process.env.NODE_ENV === 'development',
-    sourceMap: false
+    sourceMap: false,
   })
 );
 app.use(serveFavicon(join(__dirname, 'public/images', 'favicon.ico')));
 app.use(express.static(join(__dirname, 'public')));
+
+app.use(express.static(join(__dirname, 'public')));
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+
+//initialiazing express session
+app.use(
+  expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60 * 60 * 24 * 15,
+      sameSite: 'lax',
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+    },
+    store: new (connectMongo(expressSession))({
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60 * 24,
+    }),
+  })
+);
+
+//initializing passport
+require('./configure-passport');
+
+//1. mounting passport as niddleware in the app
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(bindUserToViewLocals);
 
 app.use('/', indexRouter);
 app.use('/authentication', authenticationRouter);
